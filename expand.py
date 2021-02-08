e3e82eede7d1175e7c06fc1e6ddd8bd7aab6e6d4
sam bacha <sam@freighttrust.com>
2021-02-08 06:25:27 -0800

    expand.py
    deref yaml file

---
+++ expand.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+import yaml
+
+yamlFile = yaml.safe_load(open('aggregates.yaml').read())
+
+for yf in yamlFile:
+    file_name = yf['name'].lower().replace(" ","_")
+
+    print(f"[+] Writing YAML dump for {file_name}")
+    with open(f'{file_name}.yml', 'w') as file:
+        yaml.dump(yf, file)
