740c407c70b464323e07ac245ef77652240a6d59
sam bacha <sam@freighttrust.com>
2021-02-08 06:24:40 -0800

    aggregate.py
    aggregates yaml files

---
+++ aggregates.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+import yaml
+import glob
+from os import path
+
+
+yaml_files = glob.glob(path.join(path.dirname(__file__), "*.yml"))
+yaml_loaded = [yaml.safe_load(open(yaml_file).read()) for yaml_file in yaml_files]
+
+with open('aggregates.yaml', 'w') as file:
+    yaml.dump(yaml_loaded, file)
